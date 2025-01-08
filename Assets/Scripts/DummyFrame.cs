33dd8d44036a6b8072ddc4e3c62255df674524bd
Jasur Sadikov <jasur@sadikoff.com>
2025-01-08 10:05:32 +0100

    wip 🕙 10:05 📆 2025-01-08

--- Assets/Scripts/DummyFrame.cs
+++ Assets/Scripts/DummyFrame.cs
@@ -1,8 +1,30 @@
+using System.Diagnostics;
+using System.Linq;
 using Figma;
 using Figma.Attributes;
+using UnityEngine.UIElements;
 
 namespace UITKAnimationsTest
 {
     [Uxml("Main/DummyFrame")]
-    public class DummyFrame : Element { }
+    public class DummyFrame : Element
+    {
+        [Query("SyncList")] VisualElement syncList;
+        [Query("SyncList/Element")] CustomElement syncListElement;
+
+        void Start()
+        {
+            UnityEngine.Debug.Log("Sync is invoked");
+            Stopwatch watch = Stopwatch.StartNew();
+            // syncListElement.Sync(syncList, 0, Enumerable.Range(0, 100));
+            UnityEngine.Debug.Log(watch.ElapsedMilliseconds);
+        }
+    }
+
+    public class CustomElement : SyncButtonSimple<int>
+    {
+        public new class UxmlFactory : UxmlFactory<CustomElement> { }
+
+        public override bool IsVisible(int index, int data) => true;
+    }
 }
